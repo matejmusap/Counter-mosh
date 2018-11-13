397dd0a66b0c481c5e478134ca6e2ad8c36be54d
matejmusap <matej.musap@gmail.com>
2018-11-13 14:43:10 +0100

    navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
